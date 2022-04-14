ca8f7570ed3fccf251fd4d3143cd85a3850857ca
虹輝 山本 <nanaki0203.y@gmail.com>
2022-04-14 10:45:24 +0900

    テスト

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -123,7 +123,6 @@ void GameScene::Update()
 	{
 		viewProjection_.fovAngleY -= 0.01f;
 		viewProjection_.fovAngleY = max(viewProjection_.fovAngleY, 0.1f);
-
 	}
 	viewProjection_.up = { cosf(viewAngle),sinf(viewAngle),0.0f };
 
